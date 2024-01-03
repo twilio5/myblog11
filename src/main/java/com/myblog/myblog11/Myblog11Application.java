9ee774d86a3c634cb87e771c99564a62ea8ee83b
ppankajlinkedinppankaj <ppankajplinkedin@gmail.com>
2024-01-03 18:58:34 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblog/myblog11/Myblog11Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog11/Myblog11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
